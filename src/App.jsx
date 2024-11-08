47ef7070382a13f9a7fde07ebbcb46fe993f30f1
pedrocruz <JayJMarquez05@gmail.com>
2024-11-08 13:19:58 -0600

    Error issues...

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-// src/App.jsx
+import { useState, useEffect } from "react";
+import * as petService from "./services/petService";
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const [petList, setPetList] = useState([]);
+
+  useEffect(() => {
+    const fetchPets = async () => {
+      try {
+        const pets = await petService.index();
+
+        if (pets.error) {
+          throw new Error(pets.error);
+        }
+        setPetList(pets);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchPets();
+  }, []);
+
+  return <h1>Hello World</h1>;
 };
 
 export default App;
